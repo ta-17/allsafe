b9de493af0da7a6f311b8f7805cb517bb66603b1
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-08-25 20:04:02 +1000

    Expanded the data sources descriptions.

--- frontend/allsafe/src/app/data-sources/page.tsx
+++ frontend/allsafe/src/app/data-sources/page.tsx
@@ -9,6 +9,7 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from '@/components/ui/accordion'
+import { TypographyBlockquote } from '@/typography/blockqoute'
 
 export default function DataSource() {
     return (
@@ -24,14 +25,38 @@ export default function DataSource() {
                         <AccordionItem value="item-1">
                             <AccordionTrigger>Scam Text</AccordionTrigger>
                             <AccordionContent>
-                                Our information on legitimate messages and scams
-                                are provided by{'  '}
+                                <TypographyBlockquote className="">
+                                    The SMS Spam Collection is a set of SMS
+                                    tagged messages that have been collected for
+                                    SMS Spam research. It contains one set of
+                                    SMS messages in English of 5,574 messages,
+                                    tagged acording being ham (legitimate) or
+                                    spam.
+                                </TypographyBlockquote>
+                                <br />
+                                This is a reliable dataset, it is sourced from
+                                kaggle which provide public datasets for data
+                                science and machine learning engineers. This
+                                dataset is used to power our ai model and our
+                                quizzes.{' '}
                                 <a
                                     href="https://www.kaggle.com/datasets/uciml/sms-spam-collection-dataset/data"
                                     className="underline"
                                 >
-                                    Kaggle.
+                                    Found here.
                                 </a>
+                                <br />
+                                <br />
+                                <p>The original work is provided by:</p>
+                                <p>
+                                    Almeida, T.A., GÃ³mez Hidalgo, J.M.,
+                                    Yamakami, A. Contributions to the Study of
+                                    SMS Spam Filtering: New Collection and
+                                    Results. Proceedings of the 2011 ACM
+                                    Symposium on Document Engineering
+                                    (DOCENG&apos;11), Mountain View, CA, USA,
+                                    2011.
+                                </p>
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-2">
@@ -39,14 +64,27 @@ export default function DataSource() {
                                 Data Insights (Historical Data)
                             </AccordionTrigger>
                             <AccordionContent>
-                                The data used to created our interactive graphs
-                                and charts is provided by{' '}
-                                <a
-                                    href="https://www.scamwatch.gov.au/research-and-resources/scam-statistics"
-                                    className="underline"
-                                >
-                                    Scam Watch.
-                                </a>
+                                <p>
+                                    ScamWatch is a government website which is
+                                    ran by the National Anti-Scam Centre. They
+                                    aim to helps people spot and avoid scams.
+                                    Make it easier for people to report scams
+                                    improves. Share information to disrupt
+                                    scammers, and work with government, industry
+                                    and supports law enforcement.
+                                </p>
+                                <br />
+                                <p>
+                                    This is a reliable dataset, provided by the
+                                    government. It is updated frequently on a
+                                    monthly basis. It can be{' '}
+                                    <a
+                                        href="https://www.scamwatch.gov.au/research-and-resources/scam-statistics"
+                                        className="underline"
+                                    >
+                                        found here.
+                                    </a>
+                                </p>
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-3">
@@ -54,11 +92,37 @@ export default function DataSource() {
                                 Social Media Scams
                             </AccordionTrigger>
                             <AccordionContent>
-                                The data used to detect whether social media
-                                dm&apos;s is from{' '}
-                                <a href="https://github.com/ScamSpot/scamspot_ig-comment-scraper/blob/main/data/comments-rated-plus-reported.csv">
-                                    Github.
-                                </a>
+                                <p>
+                                    {' '}
+                                    I should see a brief description for each
+                                    source, explaining its reliability,
+                                    relevance, and the role it plays in the
+                                    platform.
+                                </p>
+                                <TypographyBlockquote className="">
+                                    Collects and annotates Instagram comments in
+                                    bulk for detecting spam and scam comments on
+                                    Instagram.
+                                </TypographyBlockquote>
+                                <br />
+                                <p>
+                                    The is a reliable source. It is produced by
+                                    a individual person who works in IT. The
+                                    dataset seems to be from resources gathered
+                                    for a research paper. The frequency of
+                                    updates is unknown, with the last activity
+                                    on Github being a year ago.
+                                </p>
+                                <p>
+                                    The data used to detect whether social media
+                                    dm&apos;s, and can be{' '}
+                                    <a
+                                        className="underline"
+                                        href="https://github.com/ScamSpot/scamspot_ig-comment-scraper/blob/main/data/comments-rated-plus-reported.csv"
+                                    >
+                                        found here.
+                                    </a>
+                                </p>
                             </AccordionContent>
                         </AccordionItem>
                     </Accordion>
