778023b76b50234257a9963a8f47117c484030a4
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-08-25 16:30:50 +1000

    Added lucide react icons and craft components.

--- frontend/allsafe/src/app/page.tsx
+++ frontend/allsafe/src/app/page.tsx
@@ -5,6 +5,7 @@ import NavBar from "../components/NavBar";
 import { TypographyH1 } from "@/typography/h1";
 import { TypographyLead } from "@/typography/lead";
 import { Button } from "@/components/ui/button";
+import Feature from "@/components/hero/feature";
 
 export default function Home() {
   return (
@@ -64,24 +65,7 @@ export default function Home() {
         </a>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-      </div>
+      <Feature />
     </main>
   );
 }
---
+++ frontend/allsafe/src/components/hero/feature.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Feature() {
+    return (
+        <div>
+            <p>Img</p>
+            <div>
+                <p>Title of feature</p>
+                <p>Description of feature</p>
+            </div>
+        </div>
+    )
+}
