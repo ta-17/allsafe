1eb0d304bbbeac71564a2b8004c4b455152f6912
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-08-30 10:36:15 +1000

    Main padding fixed.

--- frontend/allsafe/src/app/layout.tsx
+++ frontend/allsafe/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
             </head>
             <body className={inter.className}>
                 <NavBar />
-                <main className="flex min-h-screen flex-col items-center justify-between p-24">
+                <main className="flex min-h-screen flex-col items-center justify-between px-8 py-24 md:px-24">
                     {children}
                 </main>
                 <Footer />
