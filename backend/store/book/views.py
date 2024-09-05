8be7c70dbbd3b31975cd08ffc8dc1b270dbd9b59
Parisa Forghani Arani <parisa.forghani@gmail.com>
2024-09-05 22:54:33 +1000

    10scam 10ok -1

--- backend/store/book/views.py
+++ backend/store/book/views.py
@@ -12,7 +12,7 @@ def random_scam_sms(request):
 
     indexes = [165, 259, 375, 834, 1688, 1765, 2645, 4571, 5028, 530, 895, 
       935, 1102, 1694, 2052, 2460, 3296, 3546, 5199, 5400]
-    indexes_plus_one = [i + 1 for i in indexes]
+    indexes_plus_one = [i - 1 for i in indexes]
 
     # Fetch 100 random rows using the generated random indexes
     data = list(ScamSMS.objects.filter(id__in=indexes_plus_one).values())
