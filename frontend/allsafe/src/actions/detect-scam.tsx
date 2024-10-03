171de3bff9224b18cda3e5f7443c70530c88491f
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-10-04 00:32:21 +1000

    Removed throw from detectScam.

--- frontend/allsafe/src/actions/detect-scam.tsx
+++ frontend/allsafe/src/actions/detect-scam.tsx
@@ -1,22 +1,18 @@
 'use server'
 
 export async function detectScam(message: string) {
-    try {
-        const response = await fetch(process.env.AI_DETECT_API!, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ text: message }),
-        })
+    const response = await fetch(process.env.AI_DETECT_API!, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ text: message }),
+    })
 
-        // if (!response.ok) {
-        //     throw new Error('API request failed')
-        // }
+    // if (!response.ok) {
+    //     throw new Error('API request failed')
+    // }
 
-        const data = await response.json()
-        return data
-    } catch (error) {
-        throw error
-    }
+    const data = await response.json()
+    return data
 }
