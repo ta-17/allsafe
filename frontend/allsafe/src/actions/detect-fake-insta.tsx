236dd3ce8ebb4800b78c3871cadf8be02061161a
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-10-06 03:26:06 +1100

    Removed Immediately invoked function expression.

--- frontend/allsafe/src/actions/detect-fake-insta.tsx
+++ frontend/allsafe/src/actions/detect-fake-insta.tsx
@@ -17,12 +17,12 @@ export async function detectFakeInstaAccount({
     isProfilePic: boolean
     isPrivate: boolean
 }) {
-    ;(async () => {
+    try {
         const worker = await createWorker('eng')
         const imageUrl = URL.createObjectURL(file)
-        // OCR with Tesseract (js)
+
+        // OCR with Tesseract.js
         const ret = await worker.recognize(imageUrl)
-        console.log('OCR: ', ret.data)
 
         // Parse username from text
         const username = ret.data.text
@@ -30,71 +30,77 @@ export async function detectFakeInstaAccount({
             .split('\n')[0]
             .split(' ')[1]
         const usernameRatio = calculateNumericRatio(username)
-        console.log('username', username, usernameRatio)
 
         // Parse fullname from text
         const name = ret.data.text.split('\n')[5]
         const numOfWords = name.split(' ').length
         const nameRatio = calculateNumericRatio(name)
-        console.log('name', name, numOfWords, nameRatio)
 
         // Check if name === username
-        let nameIsUsername = false
-        if (name.trim() === username.trim()) nameIsUsername = true
-        console.log('Is name the same as username?', nameIsUsername)
+        let nameIsUsername = 0
+        if (name.trim() === username.trim()) nameIsUsername = 1
 
         // Parse description and find length (char)
         const description = ret.data.text.split('\n').slice(7, 12).join('')
         const descriptionLength = description.length
-        console.log('description', description, descriptionLength)
 
         // Parse text for external URL
         const externalUrl = ret.data.text.split('\n').slice(0, 12).join(' ')
         const isExternalUrlPresent = checkForExternalUrl(externalUrl)
-        console.log('external url', externalUrl, isExternalUrlPresent)
 
         // Parse posts count
         const posts = ret.data.text.split('\n')[2].split(' ')[1]
         const postsAsNumber = handleModifier(posts)
-        console.log('posts', posts, postsAsNumber)
 
         // Parse followers count
         const followers = ret.data.text.split('\n')[2].split(' ')[2]
         const followersAsNumber = handleModifier(followers)
-        console.log('followers', followers, followersAsNumber)
 
         // Parse following count
         const following = ret.data.text.split('\n')[2].split(' ')[3]
         const followingAsNumber = handleModifier(following)
-        console.log('following', following, followingAsNumber)
 
         // Private account
-        const privateAccount = isPrivate
+        const privateAccount = isPrivate ? 1 : 0
 
         // Profile picture
-        const profilePic = isProfilePic
-
-        // Default
-        console.log('Tesseract: ', ret.data.text)
+        const profilePic = isProfilePic ? 1 : 0
 
         await worker.terminate()
-    })()
-    // try {
-    //     const response = await fetch(process.env.AI_DETECT_API!, {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify({ text: message }),
-    //     })
 
-    //     // if (!response.ok) {
-    //     //     throw new Error('API request failed')
-    //     // }
+        // Call API
+        const response = await fetch(
+            process.env.NEXT_PUBLIC_AI_FAKE_ACCOUNT_API!,
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    'profile pic': profilePic,
+                    'nums/length username': usernameRatio,
+                    'fullname words': numOfWords,
+                    'nums/length fullname': nameRatio,
+                    'name==username': nameIsUsername,
+                    'description length': descriptionLength,
+                    'external URL': isExternalUrlPresent,
+                    ['private']: privateAccount,
+                    '#posts': postsAsNumber,
+                    '#followers': followersAsNumber,
+                    '#follows': followingAsNumber,
+                }),
+            }
+        )
 
-    //     const data = await response.json()
-    //     return data
-    // } catch (error) {
-    //     throw error
-    // }
+        if (!response.ok) {
+            throw new Error('API request failed')
+        }
+
+        const data = await response.json()
+        console.log('Fake account detection result: ', data)
+        return data
+    } catch (error) {
+        console.error('Error detecting fake Instagram account:', error)
+        throw error
+    }
 }
