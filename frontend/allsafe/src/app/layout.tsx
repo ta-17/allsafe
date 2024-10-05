d7591e4b5a8ce3752f3a3b614ef3f52b1263cc62
ibb2 <70052141+ibb2@users.noreply.github.com>
2024-10-05 22:21:55 +1000

    overflow of html removed.

--- frontend/allsafe/src/app/layout.tsx
+++ frontend/allsafe/src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     children: React.ReactNode
 }>) {
     return (
-        <html lang="en">
+        <html lang="en" className="overflow-x-hidden">
             <head>
                 <meta
                     http-equiv="Content-Security-Policy"
